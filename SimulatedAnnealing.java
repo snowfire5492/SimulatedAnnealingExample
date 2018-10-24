f882b8938e9b722678dcb668ea03b81f556d4f74
snowfire5492 <38293287+snowfire5492@users.noreply.github.com>
2018-10-24 10:10:10 -0700

    update class

---
+++ SimulatedAnnealing.java
@@ -0,0 +1,34 @@
+public class SimulatedAnnealing extends NQueen {
+
+    double tempreture;
+
+    public SimulatedAnnealing(int boardSize, int tollerence, double tempreture) {
+        super(boardSize, tollerence);
+        this.tempreture = tempreture;
+        currentState = new SimulatedAnnealingState(boardSize);
+    }
+
+    @Override
+    public void solve() {
+        while (!isSolvedPossition(currentState)) {
+            double temperature;
+            double delta;
+            double probability;
+            double rand;
+
+
+            for (temperature = this.tempreture; (temperature > 0) && (currentState.getCost() != 0); temperature--) {
+                nextState = currentState.getNextState();
+                delta = currentState.getCost() - nextState.getCost();
+                probability = Math.exp(delta / temperature);
+                rand = Math.random();
+
+                if (delta > 0) {
+                    currentState = nextState;
+                } else if (rand <= probability) {
+                    currentState = nextState;
+                }
+            }
+        }
+    }
+}
